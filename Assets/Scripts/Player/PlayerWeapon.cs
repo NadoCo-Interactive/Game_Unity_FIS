305c5ba8ec205c2181ff8afb2e4da792579c9b2f
Nathan <nlinsley@nadocointeractive.com>
2023-06-10 14:32:26 +1000

    Added Screenshots for Github Readme under Media/

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -33,17 +33,12 @@ public class PlayerWeapon : MonoBehaviour
 
         if (Input.GetMouseButton(1))
         {
-            // .. make the ship turn to face the mouse
-
-            // Create a ray from the camera to the mouse position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            // Calculate the intersection point of the ray with the horizontal plane
             float distance;
             Plane plane = new Plane(Vector3.up, new Vector3(0, 0, 0));
             if (plane.Raycast(ray, out distance))
             {
-                // Get the world position of the intersection point
                 Vector3 worldPosition = ray.GetPoint(distance);
 
                 Vector3 targetDirection = worldPosition - shipTransform.position;
