7056a0e333c919767f4be20bdbc2d69822c88e88
Nathan <nlinsley@nadocointeractive.com>
2023-06-18 00:45:04 +1000

    Finish Checking-In Other Changes

--- Assets/Scripts/UI/InventoryCell.cs
+++ Assets/Scripts/UI/InventoryCell.cs
@@ -7,14 +7,13 @@ public class InventoryCell : MonoBehaviour
 {
     private RectTransform rect;
     private Button button;
-    private Image image;
+    private Image image, itemImage;
     private AudioSource audioSource;
-
-
     private Vector2 endSize, startSize;
     private bool isVisible = false;
-
     private bool isInitialized = false;
+
+    public IItem Item { get; private set; }
 
     void Start()
     {
@@ -28,10 +27,11 @@ public class InventoryCell : MonoBehaviour
         rect = GetComponent<RectTransform>();
         button = GetComponent<Button>();
         image = GetComponent<Image>();
+        itemImage = transform.Find("itemImage").GetComponent<Image>();
         audioSource = GetComponent<AudioSource>();
 
-        endSize = rect.sizeDelta;
-        startSize = rect.sizeDelta * 1.15f;
+        endSize = rect.localScale;
+        startSize = rect.localScale * 1.15f;
 
         isInitialized = true;
         Hide();
@@ -46,14 +46,21 @@ public class InventoryCell : MonoBehaviour
     void Update()
     {
         if (isVisible)
-            rect.sizeDelta = Vector2.Lerp(rect.sizeDelta, endSize, Time.deltaTime * 10);
+            rect.localScale = Vector2.Lerp(rect.localScale, endSize, Time.deltaTime * 10);
+    }
+
+    public void SetItem(IItem item)
+    {
+        item = Item;
+        itemImage.enabled = Item != null;
     }
 
     public void Show()
     {
         VerifyInitialize();
-        rect.sizeDelta = startSize;
+        rect.localScale = startSize;
         image.enabled = true;
+        itemImage.enabled = Item != null;
         button.interactable = true;
         isVisible = true;
         audioSource.Play();
@@ -63,6 +70,7 @@ public class InventoryCell : MonoBehaviour
     {
         VerifyInitialize();
         image.enabled = false;
+        itemImage.enabled = false;
         button.interactable = false;
         isVisible = false;
     }
