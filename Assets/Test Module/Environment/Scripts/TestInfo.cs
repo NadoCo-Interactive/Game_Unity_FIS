73b276a4457feca11361db4ea1bdc8bf1e3786c9
Nathan <nadocouniversal@gmail.com>
2023-09-10 18:45:40 +1000

    Working on TestInfo, Fixed Issues with TestName, Added Sprites for Status Icon
    + Started working on class for "ServerConnectionUnitTest"

--- Assets/Test Module/Environment/Scripts/TestInfo.cs
+++ Assets/Test Module/Environment/Scripts/TestInfo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public enum TestStatus
@@ -12,28 +13,59 @@ public class TestInfo : Singleton<TestInfo>
 {
     private TextMeshProUGUI txTestStatus;
     private Blink testStatusBlinker;
-    public TextMeshProUGUI txTestName;
+    private TextMeshProUGUI txTestName;
+    private Image imgResult;
+    private bool initialized = false;
+
+    public Sprite sprPending, sprSuccess, sprFail;
 
     void Start()
     {
-        var tfTestStatus = transform.FindRequired("txStatus");
-        testStatusBlinker = tfTestStatus.GetRequiredComponent<TextMeshProUGUI>();
-        txTestStatus = tfTestStatus.GetRequiredComponent<TextMeshProUGUI>();
-        txTestName = transform.FindRequired("txName").GetRequiredComponent<TextMeshProUGUI>();
+        verifyInitialize();
     }
 
-    public static void SetName(string name)
+    void verifyInitialize()
     {
-        Instance.txTextName.text = name;
-    } 
+        if(initialized)
+            return;
+
+        var tfTestStatus = transform.FindRequired("txStatus");
+        testStatusBlinker = tfTestStatus.GetRequiredComponent<Blink>();
+        txTestStatus = tfTestStatus.GetRequiredComponent<TextMeshProUGUI>();
+        txTestName = transform.FindRequired("txName").GetRequiredComponent<TextMeshProUGUI>();
+        imgResult = transform.FindRequired("icResult").GetRequiredComponent<Image>();
+
+        initialized = true;
+    }
 
     private static string statusToString(TestStatus status) =>
     status switch {
+        TestStatus.Running => "Running Test...",
+        TestStatus.Passed => "Test PASSED",
+        TestStatus.Failed => "Test FAILED",
         _ => throw new NotImplementedException()
     };
 
+    private static Sprite statusToSprite(TestStatus status) =>
+    status switch {
+        TestStatus.Running => Instance.sprPending.Required(),
+        TestStatus.Passed => Instance.sprSuccess.Required(),
+        TestStatus.Failed => Instance.sprFail.Required(),
+        TestStatus.None => Instance.sprPending.Required(),
+        _ => throw new NotImplementedException()
+    };
+
+    public static void SetName(string name)
+    {
+        verifyInitialize();
+        Instance.txTestName.text = name;
+    } 
+
     public static void SetStatus(TestStatus status)
     {
+        verifyInitialize();
 
+        Instance.txTestStatus.text = statusToString(status);
+        Instance.imgResult.sprite = statusToSprite(status);
     }
 }
